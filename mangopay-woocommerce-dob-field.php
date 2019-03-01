8137768b3515548cc7c7dba8a464ffc1b1be8f58
Muhammad Haris <601054+mharis@users.noreply.github.com>
2019-03-01 20:20:45 +0500

    Inital commit.

---
+++ mangopay-woocommerce-dob-field.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Mangopay WooCommerce Hide DoB field.
+ * Plugin URI:
+ * Description: Hides DoB field required for Mangopay.
+ * Version: 1.0.0
+ * Author: Haris Zulfiqar
+ * Author URI: https://hariszulfiqar.com
+ *
+ * @package Mangopay
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
