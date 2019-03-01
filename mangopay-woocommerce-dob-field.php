9d39ff42af497e6eb83648e0d6e1a18804dd44a4
Muhammad Haris <601054+mharis@users.noreply.github.com>
2019-03-01 21:00:19 +0500

    Styles and script to hide the DOB file and inject it with a dummy dob.

--- mangopay-woocommerce-dob-field.php
+++ mangopay-woocommerce-dob-field.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Mangopay WooCommerce Hide DoB field.
+ * Plugin Name: MANGOPAY WooCommerce Hide DoB field.
  * Plugin URI:
  * Description: Hides DoB field required for Mangopay.
  * Version: 1.0.0
@@ -9,9 +9,48 @@
  *
  * @package Mangopay
  * @since 1.0.0
+ * @author Haris Zulfiqar <haris@hariszulfiqar.com>
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+
+/**
+ * Injects CSS styles on checkout page to hide MANGOPAY DoB field.
+ *
+ * @since 1.0.0
+ * @author Haris Zulfiqar <haris@hariszulfiqar.com>
+ */
+function mangopay_woocommerce_hide_dob_field_styles() {
+	if ( class_exists( 'WooCommerce' ) && is_checkout() ) :
+	?>
+		<style type="text/css">
+			#user_birthday_field {
+				display: none;
+			}
+		</style>
+<?php
+	endif;
+}
+add_action( 'wp_head', 'mangopay_woocommerce_hide_dob_field_styles' );
+
+/**
+ * Injects Javascript to add a dummy DOB field to the field.
+ *
+ * @since 1.0.0
+ * @author Haris Zulfiqar <haris@hariszulfiqar.com>
+ */
+function mangopay_woocommerce_hide_dob_field_script() {
+	if ( class_exists( 'WooCommerce' ) && is_checkout() ) :
+	?>
+		<script type="text/javascript">
+			jQuery( document ).ready( function ( $ ) {
+				$( '#user_birthday_field input' ).val( 'March 01, 1990' );
+			} );
+		</script>
+<?php
+	endif;
+}
+add_action( 'wp_head', 'mangopay_woocommerce_hide_dob_field_script' );
